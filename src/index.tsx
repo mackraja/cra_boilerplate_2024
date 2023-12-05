44021e798df6f0598ad5a4ab257b4f38205628d8
mackraja <montykhanna007@hotmail.com>
2023-12-05 15:12:51 +0530

    feat: Packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import { StrictMode, Profiler } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./utils/reportWebVitals";
+import renderProfiler from "./utils/renderProfiler";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Profiler id="App" onRender={renderProfiler}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Profiler>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/utils/renderProfiler.ts
@@ -0,0 +1,19 @@
+const renderProfiler = (
+  id: string,
+  phase: string,
+  actualDuration: number,
+  baseDuration: number,
+  startTime: number,
+  commitTime: number
+): void =>
+  console.table({
+    "Profiler Data": {
+      id,
+      "Phase(mount/update/nested-update)": phase,
+      "Actual Duration(milliseconds)": actualDuration,
+      "Base Duration(milliseconds)": baseDuration,
+      "Start Time(timestamp)": startTime,
+      "Commit Time(timestamp)": commitTime,
+    },
+  });
+export default renderProfiler;
