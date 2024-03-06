7c858d7d78ffed6d9b57991aca2caa93ce8c2bef
mackraja <montykhanna007@hotmail.com>
2024-03-06 23:03:46 +0530

    feat: NPM Deps

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import reportWebVitals from "./utils/reportWebVitals";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Profiler id="App" onRender={renderProfiler}>
     <StrictMode>
